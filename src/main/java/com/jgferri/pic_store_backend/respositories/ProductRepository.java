8f6f532d236eac5b2384e399c0800f97898f935c
Ferri <ferri@moskit.com.br>
2024-11-24 18:20:19 -0300

    fix: wrong type

--- src/main/java/com/jgferri/pic_store_backend/respositories/ProductRepository.java
+++ src/main/java/com/jgferri/pic_store_backend/respositories/ProductRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductByName(String name);
-    Optional<Product> findProductById(String id);
+    Optional<Product> findProductById(Long id);
 
 }
