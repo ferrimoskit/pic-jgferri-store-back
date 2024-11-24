539cf8394abe6a33c53ebc6412c78d0e3630b712
Ferri <ferri@moskit.com.br>
2024-11-24 17:16:49 -0300

    Initial commit

---
+++ src/main/java/com/jgferri/pic_store_backend/PicStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jgferri.pic_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgferri/pic_store_backend/PicStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgferri.pic_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
